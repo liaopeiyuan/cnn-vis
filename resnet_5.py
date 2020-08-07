7c8ec70ef29d1a871577757ea82497220602d059
Alexander Liao <alexander_liao@outlook.com>
2020-08-07 15:58:54 +0800

    5 figures

--- resnet_5.py
+++ resnet_5.py
@@ -5,9 +5,13 @@ from numpy.linalg import svd
 import pickle
 import pandas as pd
 import sys
+from matplotlib import pyplot as plt
 
 networks = ['resnet'+str(x) for x in [18,34,50,101,152]]
 
+fig, ax = plt.subplots(2, 3, figsize=(30, 30))
+
+i = 0
 for NETWORK in networks:
   #Network dependent operations
   m = getattr(tv.models, NETWORK)(pretrained=True)#tv.models.resnet50(pretrained=True)
@@ -32,7 +36,6 @@ for NETWORK in networks:
 
   singular_vals = [x/y for x,y in zip(singular_vals,maxs)]
 
-  ax = None
   l = 1
   rows = []
   for x,y in zip(xcoords, singular_vals):
@@ -40,7 +43,11 @@ for NETWORK in networks:
         rows.append({'i/cout':a,'\lam/\lam_max':b,'stage':str(l)})
     l+=1
   df = pd.DataFrame(rows)
-  ax = sns.scatterplot(x='i/cout',y='\lam/\lam_max',legend='full',hue='stage', data=df)
-  ax.set_title(NETWORK)
+  sns.lineplot(x='i/cout',y='\lam/\lam_max',legend='full',hue='stage', data=df, markers=True, ax=ax[i%2][i//3])
+  ax[i%2][i//3].set_title(NETWORK)
+  i += 1
 
-ax.get_figure().savefig('{}.pdf'.format(NETWORK))
+plt.savefig('resnets.pdf')
+
+
+
